590e06a89fd99ee75df4884526f521856be4e253
TRIK-S <patrike.glima@gmail.com>
2025-02-20 23:31:34 +0000

    finaldefinity

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 int main() {
         //definindo as variáriáveis
-    char cidade[100], cidade2[100], estado[100], estado2[100], codigo[]="a01", codigo2[]= "a02";
+    char cidade[100] , cidade2[100], estado[1], estado2[1], codigo[] = "01", codigo2[] = "02";
     int populacao , populacao2 , pontostur , pontostur2;
     float area , area2 , pib , pib2;
 
-    printf("Código promeira carta: %s \n" , codigo);
+    
     //recebendo os atributos das cartas
+    
     printf("Digite o nome da sua cidade: ");
-    fgets(cidade, sizeof(cidade), stdin);
+    scanf("%s" , cidade);
 
     printf("Digite o estado da sua cidade: ");
-    fgets(estado, sizeof(estado), stdin);
+    scanf("%s" , estado);
 
     printf("Qual a população da sua cidade? ");
     scanf("%i", &populacao);
@@ -25,14 +26,13 @@ int main() {
     printf("Qual o PIB da sua cidade? ");
     scanf("%f", &pib);
     printf("\n\n");
-
-    printf("Código: %s \n" , codigo2);
+    
     //recebendo os atributos das cartas
     printf("Digite o nome da sua cidade: ");
-    fgets(cidade2, sizeof(cidade2), stdin);
+    scanf("%s" , cidade2);
 
     printf("Digite o estado da sua cidade: ");
-    fgets(estado2, sizeof(estado2), stdin);
+    scanf("%s" , estado2);
 
     printf("Qual a população da sua cidade? ");
     scanf("%i", &populacao2);
@@ -51,20 +51,20 @@ int main() {
 //imprime todos atributos cadastrados
 
     printf("O código da primeira cidade é: %s \n" , codigo);
-    printf("cidade: %s" , cidade);  
-    printf("Estado: %s" , estado); 
+    printf("Cidade: %s \n" , cidade);  
+    printf("Estado: %s \n" , estado); 
     printf("População: %i \n" , populacao); 
-    printf("ponto(s) turístico(s): %i \n" , pontostur); 
+    printf("Ponto(s) turístico(s): %i \n" , pontostur); 
     printf("Area: %.2f km² \n" , area); 
-    printf("PIB: %.2f milhões de reais \n\n\n" , pib);
+    printf("PIB: %.2f bilhões de reais \n\n" , pib);
    
     printf("O código da segunda cidade é: %s \n" , codigo2);
-    printf("Cidade: %s" , cidade2);  
-    printf("Estado: %s" , estado2); 
+    printf("Cidade: %s \n" , cidade2);  
+    printf("Estado: %s \n" , estado2); 
     printf("População: %i \n" , populacao2); 
     printf("Pontos turísticos: %i \n" , pontostur2); 
     printf("Área: %.2f km² \n" , area2); 
-    printf("PIB: %.2f milhões de reais \n\n\n" , pib2);
+    printf("PIB: %.2f bilhões de reais \n\n\n" , pib2);
     
 
     return 0;
