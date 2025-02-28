132d9c834e76a045f5d20415a2b58fb4c85fac72
TRIK-S <patrike.glima@gmail.com>
2025-02-28 03:06:36 +0000

    mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,7 @@ int main() {
     char codigo2[] = "02";
     int populacao , populacao2 , pontostur , pontostur2;
     float area , area2 , pib , pib2 , densipop , pibperc , densipop2 , pibperc2;
+    double superpoder1, superpoder2;
 
     //recebendo os atributos das cartas
     
@@ -52,8 +53,13 @@ int main() {
     printf("\n\n");
 
     densipop2 = (populacao2 / area2);
-    pibperc2 = (float)(pib2 / populacao2);    
+    pibperc2 = (float)(pib2 / populacao2);
+    
+    superpoder1 = (double)(populacao + pontostur + area + pib + pibperc - densipop);
+    superpoder2 = (double)(populacao2 + pontostur2 + area2 + pib2 + pibperc2 - densipop2);
 
+    printf("carta %s%s tem superpoder %.3f!\n\n", estado, code, superpoder1);
+    printf("carta %s%s tem superpoder %.3f!\n\n", estado2, codigo2, superpoder2);
 //imprime todos atributos cadastrados
 
     printf("O código da primeira cidade é: %s%s \n" , estado, code);
@@ -75,6 +81,19 @@ int main() {
     printf("PIB: %.2f bilhões de reais \n" , pib2);
     printf("Densidade populacional é: %.3f hab/km² \n", densipop2);
     printf("PIB percapta: %.3f reais \n", pibperc2);
+
+    printf("\n\nA população da cidade %s%s é maior do que da cidade %s%s? %i \n" , estado , code , 
+        estado2, codigo2  , (populacao > populacao2));
+    printf("A cidade %s%s tem mais pontos turísticos do que da cidade %s%s? %i \n" , estado , code , 
+            estado2, codigo2  , (pontostur > pontostur2));
+    printf("A área da cidade %s%s é maior do que a área da cidade %s%s? %i \n" , estado , code , 
+                estado2, codigo2  , (area > area2));
+    printf("O PIB da cidade %s%s é maior do que o PIB da cidade %s%s? %i \n" , estado , code , 
+                    estado2, codigo2  , (pib > pib2));
+    printf("A densidade populacional da cidade %s%s é maior do que a densidade populacional da cidade %s%s? %i \n" 
+        , estado , code , estado2, codigo2  , (densipop > densipop2));
+    printf("O PIB percapta da cidade %s%s é maior do que o PIB percapta da cidade %s%s? %i \n" , estado , code , 
+            estado2, codigo2  , (pibperc > pibperc2));            
     
     return 0;
 }
